c2d70c816cfa29983cec6e845846f85b60cea274
Robert Sturman <tssturman@gmail.com>
2021-08-27 14:52:23 -0400

    All barebones created, untested

--- StrategyDesignPattern/BinarySearch.java
+++ StrategyDesignPattern/BinarySearch.java
@@ -11,7 +11,18 @@ public class BinarySearch implements SearchBehavior {
         data.sort();
         // binary is when it goes half and half, then half, etc. 
         if (data.length()>1) {
-            
+            int halvsies = data.length()/2;
+            // fix compareto for a string
+            if (data.get(halvsies).compareTo(item) == 1) {
+                // current item > item
+                ArrayList<String> halver = data.subList(0, halvsies-1);
+                this.contains(halver, item);
+            }
+            if (data.get(halvsies).compareTo(item) == -1) {
+                // current item < item
+                ArrayList<String> halver = data.subList(halvsies+1, data.length()-1);
+                this.contains(halver, item);
+            }
         }
         if (data.length()==1 && data.get(0).equals(String)) {
             return true;
