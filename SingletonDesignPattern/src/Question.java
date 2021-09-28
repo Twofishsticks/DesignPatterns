41a54590bad9f333d6a17ebb4c5370a01bdd1b97
Robert Sturman <tssturman@gmail.com>
2021-09-28 08:27:42 -0400

    Added Framework for Classes

---
+++ SingletonDesignPattern/src/Question.java
@@ -0,0 +1,17 @@
+public class Question {
+    private String Question;
+    private String[] answers; 
+    private int CorrectAnswer;
+    public Question (String question, String ans1, String ans2, String ans3, String ans4, int CorrectAnswer) {
+
+    }
+    public String toString() {
+
+    }
+    public boolean isCorrect() {
+
+    }
+    public String getCorrectAnswer() {
+        
+    }
+}
---
+++ SingletonDesignPattern/src/TriviaGame.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TriviaGame {
+    // underline uml
+    private TriviaGame triviaGame;
+    private int score;
+    private Scanner reader;
+    private ArrayList<Questions> questions;
+    public TriviaGame() {
+
+    }
+    public TrivaiGame getInstance() {
+
+    }
+    public void play() {
+
+    }
+    private boolean playRound() {
+        
+    }
+}
