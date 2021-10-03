a0d3293f6bc423007b0c84326c24461d612f5eef
Robert Sturman <tssturman@gmail.com>
2021-10-03 09:01:24 -0400

    Full completion, includes javadoc and output

--- SingletonDesignPattern/src/Question.java
+++ SingletonDesignPattern/src/Question.java
@@ -2,6 +2,15 @@ public class Question {
     private String Question;
     private String[] answers = new String[4]; 
     private int CorrectAnswer;
+    /**
+     * Creates the Question class
+     * @param question
+     * @param ans1
+     * @param ans2
+     * @param ans3
+     * @param ans4
+     * @param CorrectAnswer
+     */
     public Question (String question, String ans1, String ans2, String ans3, String ans4, int CorrectAnswer) {
         this.Question = question;
         answers[0] = ans1;
@@ -10,6 +19,9 @@ public class Question {
         answers[3] = ans4;
         this.CorrectAnswer = CorrectAnswer;
     }
+    /**
+     * Returns the object Question as a string
+     */
     public String toString() {
         String returny = this.Question+ "\n\n";
         returny += " 1. " + answers[0] + "\n\n";
@@ -18,6 +30,11 @@ public class Question {
         returny += " 4. " + answers[3] + "\n\n";
         return returny;
     }
+    /**
+     * Checks if the user answer = the correct answer-1
+     * @param UserAnswer
+     * @return if they are the same
+     */
     public boolean isCorrect(int UserAnswer) {
         boolean correct = false;
         if (UserAnswer-1 == CorrectAnswer){
@@ -25,6 +42,10 @@ public class Question {
         }
         return correct;
     }
+    /**
+     * gets the correct answer
+     * @return correct answer
+     */
     public String getCorrectAnswer() {
         if (CorrectAnswer >= answers.length) {
             return null;
--- SingletonDesignPattern/src/TriviaGame.java
+++ SingletonDesignPattern/src/TriviaGame.java
@@ -2,41 +2,73 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TriviaGame {
-    // underline uml
     private static TriviaGame triviaGame;
     private int score; // how many games won
     private Scanner reader;
     private ArrayList<Question> questions;
+    /**
+     * Creates an instance of Trivia game, alongside everything it would need
+     * Such as: a list of questions, a scanner for user input
+     */
     private TriviaGame() {
+        reader = new Scanner(System.in);
         this.questions = DataLoader.getTriviaQuestions();
     }
+    /**
+     * Checks if Trivia game has been created, if not, creates it.
+     * @return this
+     */
     public static TriviaGame getInstance() {
         if (triviaGame == null) {
             triviaGame = new TriviaGame();
         }
         return triviaGame;
     }
+    /**
+     * Loops through the game, increasing the score for each correct round
+     * At the end of each round asks if user would like to play another
+     * After the user does not play another round, outputs total score
+     */
     public void play() {
         // while player keeps playing, play round, update score, etc.
-        String playing = "q"; // change to p once ready
+        String playing = "p"; // change to p once ready, bc infinite loop
         while (playing.toLowerCase().equals("p")){
-
-            if (playRound()) score++;
-
+            boolean increase = playRound();
+            if (increase) score++;
+            playing = reader.nextLine();
             // if input != p or q 
-            while (!(playing.toLowerCase().equals("y") || playing.toLowerCase().equals("q"))) {
+            while (!(playing.toLowerCase().equals("p") || playing.toLowerCase().equals("q"))) {
                 // error message + ask for input again
+                System.out.print("Would you like to (P)lay or (Q)uit?");
+                playing = reader.nextLine();
             }
         }
+        System.out.println("Number of games won: " + score + "\n Goodbye!");
     }
+    /**
+     * Picks a random trivia question, and displays it,
+     * Asks the user to enter an answer
+     * outputs messages based on type of answer (correct = happy, error = error, etc.)
+     * @return if the user got the question correct
+     */
     private boolean playRound() {
-        //picks random ?n, displays
-        // prompts user answer
-        // correct = good, return true
-        // false = bad, return false
-        // else = error message, false
         boolean answer = false;
+        int sizeQuestions = questions.size();
+        int randomQuestion = (int)(Math.random()*sizeQuestions);
+        System.out.println(questions.get(randomQuestion));
+        int answered = reader.nextInt();
+        if (answered > 4 || answered <= 0) {
+            System.out.print("ERROR: answer is not within the confines of the question!\n");
+            return false;
+        }
+        answer = questions.get(randomQuestion).isCorrect(answered);
 
-        return answer;
+        if (answer) {
+            System.out.print("Good job! That's correct! \n");
+            return answer;
+        } else {
+            System.out.print("Sorry, that's not right.\n");
+            return answer;
+        }
     }
 }
